c3dd8418fbfd6f0af722617f2ef18a44d908d298
Nura Programmer <nuraprogrammer@gmail.com>
2025-09-20 20:16:30 +0100

    Add daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
-
 function App() {
-  return <div>ATS Portal</div>;
+  return (
+    <div className="flex justify-center min-h-screen">
+      <div className="w-sm mx-4 my-10 flex flex-col justify-end gap-2 p-4">
+        ATS Portal
+      </div>
+    </div>
+  );
 }
 
 export default App;
